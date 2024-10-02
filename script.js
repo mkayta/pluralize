4a3f4c82d32441415b42878894f3465e9d50dd95
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-02 18:02:15 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // Good Luck 💪🏾
+function pluralize(words) {
+    let wordCount = {};
+    words.forEach(word => {
+      wordCount[word] = (wordCount[word] || 0) + 1;
+    });
+    let result = new Set(
+      words.map(word => (wordCount[word] > 1 ? word + "s" : word))
+    );
+    return Array.from(result);
+  }
+  
+  console.log(pluralize(["cat","dog","cat","mouse"]));  
+  console.log(pluralize(["car", "car", "car"]));     
+  
